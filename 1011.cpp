6dd4d234c3d7b1209005f7094dd97b258ff71c23
Suhana3003 <100698761+Suhana3003@users.noreply.github.com>
2022-06-21 23:44:15 +0530

    Add files via upload

---
+++ 1011.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    double R,volume;
+    cin >> R;
+    double pie = 3.14159;
+    volume = (4.0/3)*pie*R*R*R;
+    cout << "VOLUME = " << fixed << setprecision(3) << volume <<endl;
+    
+    
+    return 0;
+}
